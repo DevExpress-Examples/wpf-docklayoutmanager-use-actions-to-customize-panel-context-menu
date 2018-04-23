b9c1eb4209a2d937206be4bae3cb1e7ad18783c6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Customize a Panel's Context Menu via Actions
+
+
+<p>This example shows how to customize a panel's context menu via <a href="https://documentation.devexpress.com/#WPF/CustomDocument7045">customization actions</a>.<br> Menu customization actions are added via the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfDockingDockLayoutManager_ContextMenuCustomizationstopic">DockLayoutManager.ContextMenuCustomizations</a> collection.</p>
+
+<br/>
+
+
---
+++ VB/ContextMenuCustomizations_Ex/Window1.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace ContextMenuCustomizations_Ex
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub bAbout_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            MessageBox.Show("About Window")
+        End Sub
+    End Class
+End Namespace
