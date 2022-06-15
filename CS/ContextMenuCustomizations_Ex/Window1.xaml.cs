162057b2221702e995c881a6611959a856d443dd
Anthony Sam <anton.sameishchev@devexpress.com>
2022-06-15 20:52:59 +0300

    Update Window1.xaml.cs

--- CS/ContextMenuCustomizations_Ex/Window1.xaml.cs
+++ CS/ContextMenuCustomizations_Ex/Window1.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ContextMenuCustomizations_Ex {
     /// <summary>
@@ -20,7 +8,6 @@ namespace ContextMenuCustomizations_Ex {
         public Window1() {
             InitializeComponent();
         }
-
         private void bAbout_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
             MessageBox.Show("About Window");
         }
